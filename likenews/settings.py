4214cc9c7043e2fb003e96c20d2e0a7b1fc4f260
ssintro <levkleshchenko@gmail.com>
2023-06-06 20:59:04 +0200

    deploy

--- likenews/settings.py
+++ likenews/settings.py
@@ -128,7 +128,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+],
 STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static')
 
 
